601e4d6597a0e5aea033157805e80b59d26fb6a3
Keeyan Kazemi <keeyankazemi@Keeyans-MacBook-Pro.local>
2024-12-22 15:09:29 -0800

    pixelLabels array with 1st shot classification

--- src/analysis.js
+++ src/analysis.js
@@ -1,10 +1,35 @@
 let analysisCanvas = document.getElementById('analysis-canvas');
 let ctx = analysisCanvas.getContext('2d');
 
+let pixelLabels;
+
+function rgbToHex(r, g, b) {
+    return '#' + [r, g, b].map(x => {
+        const hex = x.toString(16).toUpperCase();
+        return hex.length === 1 ? '0' + hex : hex;
+    }).join('');
+}
+
+function populatePixelLabels(imageData, labelColours) {
+    pixelLabels = new Array(imageData.height)
+    for (let y = 0; y < imageData.height; y++) {
+        pixelLabels[y] = new Array(imageData.width);
+        for (let x = 0; x < imageData.width; x++) {
+            const idx = (y * imageData.width + x) * 4;
+            const hex = rgbToHex(
+                imageData.data[idx],
+                imageData.data[idx + 1],
+                imageData.data[idx + 2]
+            );
+            pixelLabels[y][x] = labelColours[hex];
+        }
+    }
+}
+
 // Request image data when window loads
 window.addEventListener('load', async () => {
     try {
-        const [receivedData, label_colours] = await Promise.all([
+        const [receivedData, labelColours] = await Promise.all([
             window.api.invoke('request-image-data'),
             window.api.invoke('get-label-colours')
         ]);
@@ -17,10 +42,14 @@ window.addEventListener('load', async () => {
                 receivedData.width,
                 receivedData.height
             );
+
+            populatePixelLabels(imageData, labelColours);
             // Debug - Put image data on canvas 
             // Unecessary, later on we'll add proper histogram data here
             ctx.putImageData(imageData, 0, 0);
-            console.log(label_colours);
+            console.log(pixelLabels)
+            console.log(imageData);
+            console.log(labelColours);
         } else {
             console.error('No image data received');
         }
